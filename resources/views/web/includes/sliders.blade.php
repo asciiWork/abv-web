1a66ac909d207663ffdf8d5bc8b4615cf753c9c0
vina babariya <vinababariya@gmail.com>
2024-03-19 23:45:27 +0530

    slider changes

--- resources/views/web/includes/sliders.blade.php
+++ resources/views/web/includes/sliders.blade.php
@@ -8,8 +8,8 @@
                                 <div class="row">
                                     <div class="col-lg-7 col-md-7">
                                         <div class="slider__content style4">      
-                                            <h2 class="slider__maintitle style4 h1">Welcome To <span class="text__secondary">ABV Tools</span></h2>
-                                            <p class="slider__subtitle style4" style="color: black;">Leading CNC machine tool Supplier and Manufacturer in India <br></p>
+                                            <h2 class="slider__maintitle style4 h1">Welcome To ABV Tools<span class="text__secondary">
+                                            Leading CNC machine tool Supplier and Manufacturer in India </span></h2>
                                         </div>
                                     </div>
                                 </div>
@@ -25,7 +25,7 @@
                                 <div class="row">
                                     <div class="col-lg-7 col-md-7">
                                         <div class="slider__content">
-                                            <h2 class="slider__maintitle style4">"Empowering Precision, Elevating Performance: <span class="text__secondary">Your CNC Tools Partner."</span></h2>
+                                            <h2 class="slider__maintitle style4">"Empowering Precision, Elevating Performance: <span class="text__secondary">Your CNC Tools Partner."<br><br></span></h2>
                                         </div>
                                     </div>
                                 </div>
@@ -41,7 +41,7 @@
                                 <div class="row">
                                     <div class="col-lg-7 col-md-7">
                                         <div class="slider__content style4">
-                                            <h2 class="slider__maintitle style4 h1">Precision Beyond Measure <span class="text__secondary"> Your Trusted CNC Tools Supplier</span></h2>
+                                            <h2 class="slider__maintitle style4 h1">Precision Beyond Measure <span class="text__secondary"> Your Trusted CNC Tools Supplier<br><br></span></h2>
                                         </div>
                                     </div>
                                 </div>
