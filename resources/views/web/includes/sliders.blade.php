af3d1057b53554ce8c8a46504b916a1faacb233e
vina babariya <vinababariya@gmail.com>
2024-03-19 23:06:04 +0530

    slider images changes

--- resources/views/web/includes/sliders.blade.php
+++ resources/views/web/includes/sliders.blade.php
@@ -1,53 +1,58 @@
 <!-- Start slider section -->
 <section class="hero__slider--section">
-    <div class="hero__slider--inner hero__slider--activation swiper">
-        <div class="hero__slider--wrapper swiper-wrapper">
-            <div class="swiper-slide ">
-                <div class="hero__slider--items style4 slider4__items--bg1" style="background-image: url('{{ asset('public/web/assets/img/slider/01_1920X730.jpg') }}');">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12 text-center">
-                                <div class="slider__content style4">
-                                    <span class="slider__subtitle slider__maintitle style4">Welcome To</span>
-                                    <h2 class="slider__maintitle style4 h1" style="color: black;">ABV Tools <span class="text__secondary"></span></h2>
-                                    <h1 class="slider__subtitle style4" style="color: black">Leading CNC machine tool Supplier and Manufacturer in India </h1>
+            <div class="hero__slider--inner hero__slider--activation swiper">
+                <div class="hero__slider--wrapper swiper-wrapper">
+                    <div class="swiper-slide ">
+                        <div class="hero__slider--items style4 slider4__items--bg1">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-7">
+                                        <div class="slider__content style4">      
+                                            <h2 class="slider__maintitle style4 h1">Welcome To <span class="text__secondary">ABV Tools</span></h2>
+                                            <p class="slider__subtitle style4" style="color: black;">Leading CNC machine tool Supplier and Manufacturer in India <br></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="hero__slider--layer__style4">
+                                <img class="slider__layer--img " src="{{asset('public/web/assets/img/slider/slider-blog-1.png')}}" alt="slider-img">
+                            </div> 
                         </div>
                     </div>
-                    <!-- <div class="hero__slider--layer__style4">
-                        <img class="slider__layer--img " src="{{ asset('public/web/assets/img/logo/ABV-logo.png') }}" alt="slider-img" align="right" width="50%">
-                    </div> --> 
-                </div>
-            </div>
-            <div class="swiper-slide ">
-                <div class="hero__slider--items style4 slider4__items--bg1" style="background-image: url('{{ asset('public/web/assets/img/slider/02_1920X730.jpg') }}');">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12 text-center">
-                                <div class="slider__content style4">
-                                    <h4 class="slider__maintitle style4 h4" style="font-size: 3.2rem;"><span class="text__secondary">"Empowering Precision, <br/>Elevating Performance: <br/>Your CNC Tools Partner."</span></h4>
+                    <div class="swiper-slide ">
+                        <div class="hero__slider--items style4 slider4__items--bg1">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-7">
+                                        <div class="slider__content">
+                                            <h2 class="slider__maintitle style4">"Empowering Precision, Elevating Performance: <span class="text__secondary">Your CNC Tools Partner."</span></h2>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="hero__slider--layer__style4 style__right">
+                                <img class="slider__layer--img " src="{{asset('public/web/assets/img/slider/slider-blog-2.png')}}" alt="slider-img">
+                            </div> 
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="swiper-slide ">
-                <div class="hero__slider--items style4 slider4__items--bg1" style="background-image: url('{{ asset('public/web/assets/img/slider/03_1920X730.jpg') }}');">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12 text-center">
-                                <div class="slider__content style4">
-                                    <h2 class="slider__maintitle style4 h1" style="font-size: 3.2rem;"><span class="text__secondary">Precision Beyond Measure<br/> Your Trusted CNC Tools Supplier</span></h2>
+                    <div class="swiper-slide ">
+                        <div class="hero__slider--items style4 slider4__items--bg1">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-7">
+                                        <div class="slider__content style4">
+                                            <h2 class="slider__maintitle style4 h1">Precision Beyond Measure <span class="text__secondary"> Your Trusted CNC Tools Supplier</span></h2>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                            <div class="hero__slider--layer__style4 style__right">
+                                <img class="slider__layer--img " src="{{asset('public/web/assets/img/slider/slider-blog-3.png')}}" alt="slider-img">
+                            </div> 
                         </div>
                     </div>
                 </div>
+                <div class="slider__pagination swiper-pagination"></div>
             </div>
-        </div>
-        <div class="slider__pagination swiper-pagination"></div>
-    </div>
-</section>
+        </section>
 <!-- End slider section -->
