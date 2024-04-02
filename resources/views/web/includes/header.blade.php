b34468aefa37852e3d9ba47f88067a498dad76c7
vina babariya <vinababariya@gmail.com>
2024-04-02 18:33:05 +0530

    display name after login on header

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -215,10 +215,7 @@ $productData = \App\Models\Product::get_Allproduct();
                                 <?php $rt=route('web.login'); ?>
                             @endif
                             <a class="header__account--btn" href="{{ $rt }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" -user">
-                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                    <circle cx="12" cy="7" r="4"></circle>
-                                </svg>
+                                {{ \Auth::user()->name}}
                                 <span class="visually-hidden">My account</span>
                             </a>
                         </li>
